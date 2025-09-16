631f8738a4dd1961e108c001e16d7829203a91d0
satyendra kumar <satyendrakumar17230@gmail.com>
2025-09-16 14:24:48 +0530

    string iteration

---
+++ string iteration.py
@@ -0,0 +1,23 @@
+W="Welcome to wscubetech"
+t=len(W)
+print(t) #21
+for a in range(t): #21
+    print(W[a])
+    
+    
+#reverse
+
+W="Welcome to wscubetech"
+W=W[-1::-1]
+t=len(W)
+print(t) #21
+for a in range(t): #21
+    print(W[a])
+
+
+
+#reverse
+print("")
+for loop in range(-1,-1,-1):#21
+    print(W[a])
+
