fb576fb72bd776c0e087489ca135bdcf0a23f6b1
satyendra kumar <satyendrakumar17230@gmail.com>
2025-12-10 22:21:09 +0530

    format

---
+++ format() method.py
@@ -0,0 +1,13 @@
+w="welcome {} to {} Wscubetech".format("hello",20);
+print(w)
+
+w="welcome {1} to {0} Wscubetech".format("hello",20);
+print(w)
+
+w="welcome {a} to {b} wscube".format(a=30,b=40)
+print(w)
+
+w="welcome {b:} to {a} wscube".format(a=30,b=40)
+print(w)
+w="welcome {b:^10} to {a:^10} wscube".format(a=30,b=40)
+print(w)
