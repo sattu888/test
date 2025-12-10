2ef9e998b028493393786f004731b23f0665e8b9
satyendra kumar <satyendrakumar17230@gmail.com>
2025-12-10 10:11:22 +0530

    string function lower upper title capitalize

---
+++ string function lower upper tittle capitalize.py
@@ -0,0 +1,20 @@
+
+#lover case()
+w="Welcome to Wscubetch"
+n=w.lower()
+print(n)
+
+#upper case()
+w="Welcome to Wescubetech"
+n=w.upper()
+print(n)
+
+#tittle case()
+w="Welcome to Wescubetech"
+t=w.title()
+print(t)
+
+#capitalize()
+w="welcome to Wscubetube"
+c=w.capitalize()
+print(c)
